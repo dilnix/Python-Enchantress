2b360e588afc6a94c7217db04d269f88a3c7043b
dilnix <dilnix@gmail.com>
2021-01-15 18:31:28 +0200

    added second unittest for employee

---
+++ homework/tests-hw/unittest_simple_employee.py
@@ -0,0 +1,36 @@
+#!/bin/python
+
+import unittest
+from unittest.mock import patch
+from homework.tests_simple_employee import Employee
+
+
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        self.testdata = Employee('Dmytro', 'Filippov', 1200)
+
+    def test_email(self):
+        self.assertEqual(self.testdata.email, 'Dmytro.Filippov@email.com')
+
+    def test_fullname(self):
+        self.assertEqual(self.testdata.fullname, 'Dmytro Filippov')
+
+    def test_apply_raise(self):
+        self.testdata.apply_raise()
+        self.assertEqual(self.testdata.pay, 1260)
+
+    def test_monthly_schedule(self):
+        with patch('homework.tests_simple_employee.requests.get') as requests:
+            requests.return_value.ok = True
+            requests.return_value.text = 'some output'
+            self.assertEqual(self.testdata.monthly_schedule(
+                'January'), 'some output')
+
+        with patch('homework.tests_simple_employee.requests.get') as requests:
+            requests.return_value.ok = False
+            self.assertEqual(self.testdata.monthly_schedule(
+                'January'), 'Bad Response!')
+
+
+if __name__ == '__main__':
+    unittest.main()
