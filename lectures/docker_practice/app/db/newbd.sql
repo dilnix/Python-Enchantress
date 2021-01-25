e1954703e132806a5d68178621ac3a69e669f3af
dilnix <dilnix@gmail.com>
2021-01-25 20:01:05 +0200

    first step with practice on lecture

---
+++ lectures/docker_practice/app/db/newbd.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE knights;
+USE knights;
+
+CREATE TABLE kingdom_colors (
+    name VARCHAR(255),
+    color VARCHAR(255)
+);
+
+INSERT INTO kingdom_colors (name, color) VALUES ('mordor', 'black');
+INSERT INTO kingdom_colors (name, color) VALUES ('groomers', 'blue');
+INSERT INTO kingdom_colors (name, color) VALUES ('traders', 'yellow');
