6545e2b92d1c34e4da515e97579c9b0c0241d00e
dilnix <dilnix@gmail.com>
2021-02-09 00:02:20 +0200

    code made on lecture

---
+++ homework/flask_practice_hw/test_AMZ_killer.py
@@ -0,0 +1,45 @@
+from homework.flask_practice_hw.AMZ_killer import amz_killer as app
+from freezegun import freeze_time
+import pytest
+
+
+@pytest.fixture
+def store_app():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        return client
+
+
+@freeze_time('2021-02-08 14:16:41')
+def test_create_user(store_app):
+    response = store_app.post(
+        '/users',
+        json={
+            "name": "Illia",
+            "email": "illia.sukonnik@gmail.com"
+        })
+    assert response.status_code == 201
+    assert response.json == {
+        "user_id": 1,
+        "registration_timestamp": '2021-02-08T14:16:41'
+    }
+
+    user_id = response.json['user_id']
+    response = store_app.get(f'/users/{user_id}')
+
+    assert response.status_code == 200
+    assert response.json == {
+        "name": "Illia",
+        "email": "illia.sukonnik@gmail.com",
+        "user_id": user_id,
+        "registration_timestamp": '2021-02-08T14:16:41'
+    }
+
+
+def test_get_user_no_such_user(store_app):
+    response = store_app.get('/users/2')
+
+    assert response.status_code == 404
+    assert response.json == {
+        "error": "no such user with id 1"
+    }
