7fbe1bbd6e595b6d1931ce67d3b9d0a2e528b45d
dilnix <dilnix@gmail.com>
2021-02-14 22:21:26 +0200

    fully finished hw08 with creating user/cart functions and all tests

--- homework/flask_practice_hw/AMZ_killer.py
+++ homework/flask_practice_hw/AMZ_killer.py
@@ -5,7 +5,6 @@ amz_killer = Flask(__name__)
 
 USERS_DATABASE = {}
 CARTS_DATABASE = {}
-CART_DETAILS_DATABASE = {}
 user_counter = 1
 cart_counter = 1
 
@@ -15,16 +14,21 @@ class NoSuchUser(Exception):
         self.user_id = user_id
 
 
+class NoSuchCart(Exception):
+    def __init__(self, cart_id):
+        self.cart_id = cart_id
+
+
 @amz_killer.route('/users', methods=["POST"])
 def create_user():
     global user_counter
     user = request.json
     user['user_id'] = user_counter
     response = {
-        "registration_timestamp": datetime.now().isoformat(),
-        "user_id": user_counter
+        "user_id": user_counter,
+        "registration_timestamp": datetime.now().isoformat()
     }
-    user["registration_timestamp"] = response["registration_timestamp"]
+    user['registration_timestamp'] = response['registration_timestamp']
     USERS_DATABASE[user_counter] = user
 
     user_counter += 1
@@ -36,6 +40,13 @@ def create_user():
 def no_such_user_handler(e):
     return {
         "error": f"no such user with id {e}"
+    }, 404
+
+
+@amz_killer.errorhandler(NoSuchCart)
+def no_such_cart_handler(e):
+    return {
+        "error": f"no such cart with id {e}"
     }, 404
 
 
@@ -64,12 +75,70 @@ def set_user(user_id):
 
 @amz_killer.route('/users/<int:user_id>', methods=['DELETE'])
 def remove_user(user_id):
+    global user_counter
     try:
         USERS_DATABASE.pop(user_id)
     except KeyError:
         raise NoSuchUser(user_id)
     else:
+        user_counter -= 1
         return {"status": "success"}, 200
+
+
+@amz_killer.route('/carts', methods=['POST'])
+def create_cart():
+    global cart_counter
+    cart = request.json
+    try:
+        cart['cart_id'] = cart_counter
+        response = {
+            "cart_id": cart_counter,
+            "creation_time": datetime.now().isoformat()
+        }
+        cart['creation_time'] = response['creation_time']
+        CARTS_DATABASE[cart_counter] = cart
+    except KeyError:
+        raise NoSuchUser(cart['user_id'])
+    else:
+        cart_counter += 1
+        return response, 201
+
+
+@amz_killer.route('/carts/<int:cart_id>')
+def get_cart(cart_id):
+    try:
+        cart = CARTS_DATABASE[cart_id]
+    except KeyError:
+        raise NoSuchCart(cart_id)
+    else:
+        return cart
+
+
+@amz_killer.route('/carts/<int:cart_id>', methods=['PUT'])
+def set_cart(cart_id):
+    data = request.json
+    try:
+        cart = CARTS_DATABASE[cart_id]
+        if cart['user_id'] == data['user_id']:
+            cart['products'] = data['products']
+        else:
+            raise NoSuchUser(data['user_id'])
+    except KeyError:
+        raise NoSuchCart(cart_id)
+    else:
+        return {'status': 'success'}, 200
+
+
+@amz_killer.route('/carts/<int:cart_id>', methods=['DELETE'])
+def remove_cart(cart_id):
+    global cart_counter
+    try:
+        CARTS_DATABASE.pop(cart_id)
+    except KeyError:
+        raise NoSuchCart(cart_id)
+    else:
+        cart_counter -= 1
+        return {'status': 'success'}, 200
 
 
 if __name__ == '__main__':
--- homework/flask_practice_hw/test_AMZ_killer.py
+++ homework/flask_practice_hw/test_AMZ_killer.py
@@ -1,3 +1,4 @@
+from flask import json
 from homework.flask_practice_hw.AMZ_killer import amz_killer as app
 from freezegun import freeze_time
 import pytest
@@ -17,7 +18,8 @@ def test_create_user(store_app):
         json={
             "name": "Illia",
             "email": "illia.sukonnik@gmail.com"
-        })
+        }
+    )
     assert response.status_code == 201
     assert response.json == {
         "user_id": 1,
@@ -51,7 +53,8 @@ def test_set_user(store_app):
         json={
             "name": "Illia",
             "email": "illia.sukonnik@example.com",
-        })
+        }
+    )
 
     assert response.status_code == 200
 
@@ -60,7 +63,8 @@ def test_set_user(store_app):
         json={
             "name": "Illia",
             "email": "illia.sukonnik@example.com",
-        })
+        }
+    )
 
     assert response.status_code == 404
     assert response.json == {
@@ -78,4 +82,123 @@ def test_remove_user(store_app):
     assert response.status_code == 404
     assert response.json == {
         "error": "no such user with id 3"
+    }
+
+
+@freeze_time('2021-02-08 14:16:41')
+def test_create_cart(store_app):
+    response = store_app.post(
+        '/carts',
+        json={
+            "user_id": 1,
+            "products": [
+                {
+                    "product": 'Book: how to stop be boring',
+                    "price": 500,
+                },
+                {
+                    "product": 'fireworks',
+                    "price": 1500,
+                }
+            ]
+        }
+    )
+    assert response.status_code == 201
+    assert response.json == {
+        "cart_id": 1,
+        "creation_time": '2021-02-08T14:16:41'
+    }
+
+    cart_id = response.json['cart_id']
+    response = store_app.get(f'/carts/{cart_id}')
+
+    assert response.status_code == 200
+    assert response.json == {
+        "cart_id": cart_id,
+        "user_id": 1,
+        "creation_time": '2021-02-08T14:16:41',
+        "products": [
+            {
+                "product": 'Book: how to stop be boring',
+                "price": 500,
+            },
+            {
+                "product": 'fireworks',
+                "price": 1500,
+            }
+        ]
+    }
+
+
+def test_get_cart_no_such_cart(store_app):
+    response = store_app.get('/carts/2')
+
+    assert response.status_code == 404
+    assert response.json == {
+        "error": "no such cart with id 2"
+    }
+
+
+def test_set_cart(store_app):
+    response = store_app.put(
+        '/carts/1',
+        json={
+            "user_id": 1,
+            "products": [
+                {
+                    "product": 'fireworks',
+                    "price": 1500,
+                }
+            ]
+        }
+    )
+
+    assert response.status_code == 200
+
+    response = store_app.put(
+        '/carts/3',
+        json={
+            "user_id": 1,
+            "products": [
+                {
+                    "product": 'fireworks',
+                    "price": 1500,
+                }
+            ]
+        }
+    )
+
+    assert response.status_code == 404
+    assert response.json == {
+        "error": "no such cart with id 3"
+    }
+
+    response = store_app.put(
+        '/carts/1',
+        json={
+            "user_id": 3,
+            "products": [
+                {
+                    "product": 'fireworks',
+                    "price": 1500,
+                }
+            ]
+        }
+    )
+    assert response.status_code == 404
+    assert response.json == {
+        "error": "no such user with id 3"
+    }
+
+
+def test_remove_cart(store_app):
+    response = store_app.delete('/carts/1')
+
+    assert response.status_code == 200
+
+    response = store_app.delete('/carts/3')
+
+    assert response.status_code == 404
+    assert response.json == {
+        "error": "no such cart with id 3"
     }
